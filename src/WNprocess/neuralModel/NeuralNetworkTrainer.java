e79a7d12d59174d6106009ec2ee21cfdfd093fea
Michal <michal@brzezinski.net>
2019-03-17 13:10:49 +0100

    multithreading and live act refractoring

--- src/WNprocess/neuralModel/NeuralNetworkTrainer.java
+++ src/WNprocess/neuralModel/NeuralNetworkTrainer.java
@@ -4,11 +4,11 @@ import MainProgram.Interface;
 import Persistence.Persistence;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,25 +20,24 @@ import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
 import org.neuroph.core.events.LearningEvent;
 import org.neuroph.core.events.LearningEventListener;
-import org.neuroph.core.learning.LearningRule;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.learning.BackPropagation;
 import org.neuroph.util.TransferFunctionType;
 
 public class NeuralNetworkTrainer {
 
-    private final NeuralNetwork neuralNetwork;
+    private final MultiLayerPerceptron neuralNetwork;
     private static final String NEURAL_NETWORK_STORAGE_FILENAME = "neuralnetwork_state.nnet";
     private final DataSet trainingSet = new DataSet(100, 1);
     private final LearningEventListener listener;
-    private final LearningRule lr;
-    private final ExecutorService executor;
-    private final BackPropagation backPropagation = new BackPropagation();
-    private final AtomicBoolean isLearning;
+    private final ExecutorService trainExecutor;
+    private final AtomicBoolean busy = new AtomicBoolean();
+    private ExecutorService askExecutor;
+    private final BackPropagation backPropagation;
 
     public NeuralNetworkTrainer() {
         if (new File(NEURAL_NETWORK_STORAGE_FILENAME).exists()) {
-            neuralNetwork = NeuralNetwork.createFromFile(NEURAL_NETWORK_STORAGE_FILENAME);
+            neuralNetwork = (MultiLayerPerceptron) NeuralNetwork.createFromFile(NEURAL_NETWORK_STORAGE_FILENAME);
             System.out.println("loading neural network from file: " + NEURAL_NETWORK_STORAGE_FILENAME);
         } else {
             neuralNetwork = new MultiLayerPerceptron(TransferFunctionType.GAUSSIAN, 100, 100, 50, 10, 1);
@@ -46,33 +45,28 @@ public class NeuralNetworkTrainer {
             System.out.println(" ---------> NEW instance of neural network created - no storage found!");
         }
         listener = (e) -> listener(e);
-        lr = neuralNetwork.getLearningRule();
-        lr.addListener(listener);
-        executor = Executors.newSingleThreadExecutor();
-        backPropagation.setMaxIterations(100);
-        isLearning = new AtomicBoolean(false);
+        backPropagation = (BackPropagation) neuralNetwork.getLearningRule();
+        backPropagation.addListener(listener);
+        backPropagation.setMaxIterations(10);
+        trainExecutor = Executors.newSingleThreadExecutor();
+        askExecutor = Executors.newSingleThreadExecutor();
     }
 
-    private void useSomanticLibraryToLearn() {
-        setText(" [meditating] ");
-        if (!isLearning.get()) {
-            Interface.getSomanticFacade()
-                    .getSomanticRepo()
-                    .entrySet()
-                    .forEach(a -> a.getValue()
-                    .getAffects()
-                    .forEach(
-                            v -> addToLearningDataset(v, a.getValue().hashCode())
-                    ));
-            learn();
-            Interface.getNnStateLabel().setText("");
-        }
-    }
-
-    private void listener(LearningEvent e) {
-        LearningEvent.Type t = e.getEventType();
-        setText(" ["+t.name().toLowerCase().replace("_", " ")+"] ");
-        Persistence.saveNeuralNetwork(neuralNetwork, NEURAL_NETWORK_STORAGE_FILENAME);
+    public void useSomanticLibraryToLearn(JToggleButton b) {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            setText("meditation");
+            while (b.isSelected()) {
+                if (!busy.get()) {
+                    useSomanticLibraryToLearn();
+                    setText(" [repeat] ");
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Interface.setMessage(ex.getMessage());
+                }
+            }
+        });
     }
 
     public int getSize() {
@@ -80,49 +74,59 @@ public class NeuralNetworkTrainer {
     }
 
     public void addToLearningDataset(List<Integer> a, int hashCode) {
-        addRowToLearningDataset(stringify(a), hashCode);
+        trainExecutor.submit(() -> {
+            addRowToLearningDataset(stringify(a), hashCode);
+        });
     }
 
     public void stopLearning() {
-        neuralNetwork.stopLearning();
-        executor.submit(() -> trainingSet.clear());
+        try {
+            setText("stopping");
+            neuralNetwork.stopLearning();
+            backPropagation.stopLearning();
+            trainExecutor.awaitTermination(2, TimeUnit.SECONDS);
+            Persistence.saveNeuralNetwork(neuralNetwork, NEURAL_NETWORK_STORAGE_FILENAME);
+        } catch (InterruptedException ex) {
+            setText(ex.getMessage());
+        }
     }
 
     public void learn() {
-        if (!isLearning.get()) {
-            executor.execute(() -> {
-                isLearning.set(true);
+        setText("learn");
+        if (!busy.get()) {
+            trainExecutor.execute(() -> {
+                setText("learning");
                 neuralNetwork.learn(trainingSet, backPropagation);
-                lr.learn(trainingSet);
+                backPropagation.learn(trainingSet);
                 trainingSet.clear();
-                isLearning.set(false);
+                setText("done");
+                Persistence.saveNeuralNetwork(neuralNetwork, NEURAL_NETWORK_STORAGE_FILENAME);
             });
         }
     }
 
-    private void setText(String text) {
-        JLabel l = Interface.getNnStateLabel();
-        String t = l.getText().replace(text, "").concat(text);
-        if(t.length()>40)
-            l.setText(text);
-        else
-            l.setText(t.replace("  ", " "));
+    public Long ask(String affect) throws ExecutionException, InterruptedException {
+        setText("ask");
+        stopLearning();
+        return askExecutor.submit(() -> {
+            setText(" [asking] ");
+            Interface.getNnStateLabel().setText("");
+            Interface.setMessage("asking for: " + affect);
+            neuralNetwork.setInput(stringToDoubleArray(affect));
+            neuralNetwork.calculate();
+            double[] networkOutput = neuralNetwork.getOutput();
+            Double output = ((networkOutput[0] - 1) * Integer.MAX_VALUE) % Integer.MAX_VALUE;
+            setText(" [output: " + output + "] ");
+            Long response = output.longValue();
+            Interface.setMessage("found response: " + response);
+            setText("[response: " + response.toString() + "]");
+            return response;
+        }).get();
     }
 
-    public Long ask(String affect) throws ExecutionException, InterruptedException {
-        stopLearning();
-        setText(" [asking] ");
-        Interface.getNnStateLabel().setText("");
-        Interface.setMessage("asking for: " + affect);
-        neuralNetwork.setInput(affectToDoubleArray(affect));
-        neuralNetwork.calculate();
-        double[] networkOutput = neuralNetwork.getOutput();
-        Double output = ((networkOutput[0] - 1) * Integer.MAX_VALUE) % Integer.MAX_VALUE; 
-        setText(" [output: "+ output +"] ");
-        Long response = output.longValue();
-        Interface.setMessage("found response: " + response);
-        setText("[response: " + response.toString() + "]");
-        return response;
+    private void listener(LearningEvent e) {
+        LearningEvent.Type t = e.getEventType();
+        setText(" [" + t.name().toLowerCase().replace("_", " ") + "] ");
     }
 
     private String stringify(List<Integer> a) {
@@ -132,19 +136,14 @@ public class NeuralNetworkTrainer {
     }
 
     private void addRowToLearningDataset(String affect, Integer somanticWordId) {
-        executor.execute(() -> {
-            double[] affectToTrain = affectToDoubleArray(affect);
-            double[] result = new double[1];
-            result[0] = somanticWordId.doubleValue() / Integer.MAX_VALUE;
-            trainingSet.addRow(new DataSetRow(affectToTrain, result));
-            //setText(" [dataset] " + result[0]);
-            if (!isLearning.get()) {
-                learn();
-            }
-        });
+        setText("adding row to learning dataset");
+        double[] affectToTrain = stringToDoubleArray(affect);
+        double[] result = new double[1];
+        result[0] = somanticWordId.doubleValue() / Integer.MAX_VALUE;
+        trainingSet.addRow(new DataSetRow(affectToTrain, result));
     }
 
-    private double[] affectToDoubleArray(String affect) {
+    private double[] stringToDoubleArray(String affect) {
         String[] arrayAffect = affect.split(",");
         double[] doublearray = new double[arrayAffect.length];
         for (int i = 0; i < arrayAffect.length; i++) {
@@ -164,20 +163,42 @@ public class NeuralNetworkTrainer {
         return 0L;
     }
 
-    public void useSomanticLibraryToLearn(JToggleButton b) {
-        Executors.newSingleThreadExecutor().execute(() -> {
-            while (b.isSelected()) {
-                if (!isLearning.get()) {
-                    useSomanticLibraryToLearn();
-                }
+    private void useSomanticLibraryToLearn(){
+        try {
+            trainExecutor.submit(() -> {
+                Interface.getSomanticFacade()
+                        .getSomanticRepo()
+                        .entrySet()
+                        .forEach(a -> a.getValue()
+                                .getAffects()
+                                .forEach(
+                                        v -> addRowToLearningDataset(stringify(v), a.getValue().hashCode())
+                                ));
                 Interface.getNnStateLabel().setText("");
-                setText(" [repeat] ");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException ex) {
-                    Interface.setMessage(ex.getMessage());
-                }
-            }
-        });
+            }).get();
+            learn();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(NeuralNetworkTrainer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExecutionException ex) {
+            Logger.getLogger(NeuralNetworkTrainer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void setText(String text) {
+        JLabel l = Interface.getNnStateLabel();
+        String t = l.getText().concat(text);
+        if (l.getText().equals(text)) {
+            t = "";
+        } else if (l.getText().contains(text)) {
+            t = text;
+
+        } else {
+            System.out.println(t);
+        }
+        if (t.length() > 40) {
+            l.setText(text);
+        } else {
+            l.setText(t.replace("  ", " "));
+        }
     }
 }
